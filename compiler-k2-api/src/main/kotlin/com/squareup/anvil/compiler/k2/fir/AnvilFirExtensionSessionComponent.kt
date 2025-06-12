bee3fce244d4a78e6e89ed9f79904d62f5ecfb79
Rick Busarow <rickbusarow@gmail.com>
2025-06-12 16:36:34 -0500

    refactor: removing unused code

--- compiler-k2-api/src/main/kotlin/com/squareup/anvil/compiler/k2/fir/AnvilFirExtensionSessionComponent.kt
+++ compiler-k2-api/src/main/kotlin/com/squareup/anvil/compiler/k2/fir/AnvilFirExtensionSessionComponent.kt
@@ -5,40 +5,9 @@ import com.squareup.anvil.annotations.internal.InternalAnvilApi
 import org.jetbrains.kotlin.fir.FirSession
 import org.jetbrains.kotlin.fir.caches.FirLazyValue
 import org.jetbrains.kotlin.fir.caches.firCachesFactory
-import org.jetbrains.kotlin.fir.caches.getValue
 import org.jetbrains.kotlin.fir.extensions.FirExtensionSessionComponent
 import org.jetbrains.kotlin.fir.extensions.predicate.LookupPredicate
 import org.jetbrains.kotlin.fir.extensions.predicateBasedProvider
-import org.jetbrains.kotlin.util.ComponentArrayOwner
-import org.jetbrains.kotlin.util.TypeRegistry
-
-private class DelegatingComponentArrayOwner(
-  override val typeRegistry: TypeRegistry<AnvilSessionComponent, AnvilSessionComponent>,
-) : ComponentArrayOwner<AnvilSessionComponent, AnvilSessionComponent>()
-
-public class MyAnvilSessionComponent(session: FirSession) : AnvilSessionComponent(session)
-
-// public val AnvilFirContext.myAnvilSessionComponent: MyAnvilSessionComponent by AnvilFirContext.sessionComponentAccessor()
-
-public class AnvilFirExtensionSessionComponent2(session: FirSession) :
-  FirExtensionSessionComponent(session) {
-
-  protected inline fun <T, R> FirLazyValue<T>.map(
-    crossinline transform: (T) -> R,
-  ): FirLazyValue<R> = session.firCachesFactory.createLazyValue { transform(this.getValue()) }
-
-  protected inline fun <T> lazyValue(crossinline initializer: () -> T): FirLazyValue<T> {
-
-    return session.firCachesFactory.createLazyValue { initializer() }
-  }
-
-  protected inline fun <reified T> lazySymbols(predicate: LookupPredicate): FirLazyValue<List<T>> {
-    return lazyValue {
-      session.predicateBasedProvider.getSymbolsByPredicate(predicate)
-        .filterIsInstance<T>()
-    }
-  }
-}
 
 public abstract class AnvilFirExtensionSessionComponent(
   session: FirSession,
@@ -59,13 +28,6 @@ public abstract class AnvilFirExtensionSessionComponent(
         .filterIsInstance<T>()
     }
   }
-
-  // protected inline fun <reified T> lazySymbols(predicate: DeclarationPredicate): FirLazyValue<List<T>> {
-  //   return lazyValue {
-  //     session.predicateBasedProvider.getSymbolsByPredicate(predicate)
-  //       .filterIsInstance<T>()
-  //   }
-  // }
 }
 
 @InternalAnvilApi
